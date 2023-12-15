7174e57adfcadeca93b87cff037921b86dd24c07
Dr.Miao <53283006+JunzMiao@users.noreply.github.com>
2023-12-15 20:16:10 +0800

    Update lenet.py

--- model/lenet.py
+++ model/lenet.py
@@ -7,27 +7,31 @@ class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
-            nn.MaxPool2d(kernel_size=2)
+            nn.Conv2d(1, 6, kernel_size=5, padding=2),
+            nn.Sigmoid(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
-            nn.MaxPool2d(kernel_size=2)
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.Sigmoid(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
         )
         self.fc1 = nn.Sequential(
-            nn.Linear(in_features=4 * 4 * 16, out_features=120)
+            nn.Flatten(),
+            nn.Linear(16 * 5 * 5, 120),
+            nn.Sigmoid(),
         )
         self.fc2 = nn.Sequential(
-            nn.Linear(in_features=120, out_features=84)
+            nn.Linear(120, 84),
+            nn.Sigmoid(),
         )
         self.fc3 = nn.Sequential(
-            nn.Linear(in_features=84, out_features=10)
+            nn.Linear(84, 10)
         )
 
     def forward(self, input):
         conv1_output = self.conv1(input)
         conv2_output = self.conv2(conv1_output)
-        conv2_output = conv2_output.reshape((-1, 4 * 4 * 16))
         fc1_output = self.fc1(conv2_output)
         fc2_output = self.fc2(fc1_output)
         fc3_output = self.fc3(fc2_output)
